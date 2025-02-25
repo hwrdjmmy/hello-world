761741fe90765ef0f343911585947788003820d8
hwrdjmmy <35844980+hwrdjmmy@users.noreply.github.com>
2025-02-25 03:28:19 +0000

    Added a api call that works from CDC website

---
+++ apicall.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("data.cdc.gov", None)
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("ynw2-4viq", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+
+print(list(results_df.columns))
